170dc1aefb3958c97df80c28891a5915e36b3733
Chang-Eop Kim <eopchang@gmail.com>
2017-05-04 01:08:47 +0900

    1. 관심 본초로 검색되는  펍메드 상의 모든 논문 title, abstract 추출 2. 추출된 문서들중 예측된 질병명(+ MeSH 유의어들(entry terms))이 포함된 문서가 얼마나 되는지 count. 3. 질병명(+ MeSH 유의어들(entry terms)) 자체로 pubmed에서 검색되는 문서의 숫자 대비, 2의 계산결과 백분율 산출.
    일반적으로 잘 작동하는지 확인이 필요함.

---
+++ PubCrawling4NetPham_results.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  4 00:52:50 2017
+
+@author: Chang-Eop
+"""
+
+import pubmed_crawler as pc
+
+#펍메드에서 검색할 본초
+term_herb = 'ginseng' 
+pheno_query = 'acute migraine'
+
+results = pc.search(term_herb)
+id_list = pc.results['IdList']
+papers = pc.fetch_details(id_list)
+
+#본초로 검색돈 문서들의 제목, 초록에서 검색할 질병.  (경로 등의 표현형도 괜찮은지 아직 확인 X)
+terms_phenotypes = pc.MeSH_terms(pheno_query) #MeSH entry terms 리스트 구성
+
+#terms_phenotypes 등장한 title, abstract 갯수 초기값.
+N_title = 0
+N_abstract = 0
+
+for i, j in enumerate(id_list):
+    print(i+1, ' of ', len(id_list))
+    
+    article = papers['PubmedArticle'][i]['MedlineCitation']['Article']
+    Title = article['ArticleTitle']
+    
+    if 'Abstract' in list(article.keys()):
+        Abstract = article['Abstract']['AbstractText'][0]
+    else:
+        print('!!!!!!!!!!!!!!!!!! no ABSTACT!!!!!!!!!') #초록 없는 경우
+    
+    
+    k = 0
+    while k < len(terms_phenotypes): #하나라도 찾았거나, 끝까지 뒤졌는데 못찾은 경우
+        term_pheno = terms_phenotypes[k]
+        if Title.lower().find(term_pheno) == -1:#해당 단어가 없을시 -1 반환.
+            k+=1
+        else:
+            N_title += 1
+            k = len(terms_phenotypes) #찾은 경우 while 벗어나기 위해.
+    k=0                 
+    while k < len(terms_phenotypes): #하나라도 찾았거나, 끝까지 뒤졌는데 못찾은 경우
+        term_pheno = terms_phenotypes[k]
+        if Abstract.lower().find(term_pheno) == -1:#해당 단어가 없을시 -1 반환.
+            k+=1
+        else:
+            N_abstract += 1
+            k = len(terms_phenotypes) #찾은 경우 while 벗어나기 위해.        
+            
+
+
+
+
+print('N_title: ',N_title,'\n','N_abstract: ',N_abstract)
+print ('\r', N_abstract,' out of',len(id_list),term_herb,\
+       '-related article(s) include(s)', "'",pheno_query,"'",'and its entry terms')
+
+
+
+#본초 관련하여 검색할 질병 자체(와 관련 MeSH의 entry terms)를 포함하는 article 갯수 조사.
+total_ids = []
+for i, q in enumerate(terms_phenotypes):
+    print(i+1,"out of", len(terms_phenotypes), 'MeSH terms')
+    
+    results_2 = pc.search(q)
+    id_list_2 = results_2['IdList']
+    total_ids.extend(id_list_2)
+
+total_counts = len(list(set(total_ids)))
+print(total_counts)
+
+#질병 관련 총 문서중 몇프로가 해당 본초 관련된것인지. (%)
+print(N_abstract/total_counts*100, "%")
