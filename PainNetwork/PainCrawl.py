f0c593e003d79544c2246c6b1e19f48f4ec3f491
Chang-Eop Kim <eopchang@gmail.com>
2017-05-22 16:40:27 +0900

    단순 주석 보강

--- PainNetwork/PainCrawl.py
+++ PainNetwork/PainCrawl.py
@@ -4,28 +4,35 @@
 Created on Thu May 18 15:34:18 2017
 
 @author: Chang-Eop
+
+parameter: query (str), size (int)
+output: results_2 (list)
+
+query에 대한 문헌정보 결과들을 results_2(list)에 담음.
+수천-수십만건의 정보를 fetching하는 동안 반복적으로 네트워크 에러 발생
+스크립트의 문제가 아니라 NCBI 서버에서 제한하는 것으로 시간대, 접속자의  IP 등 여러가지가 영향을 미침
+에러로 중단시 지속적으로 다시 시도해야 하므로 전체  fetching할 문서들을 size의 chunk로 나눈후 chunk 단위로 루프 돌려 패칭
+에러 발생시 ###FETCHING###이하의 for loop 에서 range의 시작점(최초 시작시엔 0으로 셋팅)만 에러 당시의 i 값으로 변경하여
+다시 수행하면 됨. 
 """
-
-import pubmed_crawler as pc
-from Bio import Entrez
-import pandas as pd
-import numpy as np
-
-Entrez.email = 'eopchang@gmail.com'
 
 query = 'pain[majr]' # [majr]:to search a MeSH heading that is a major topic of an article
 size = 1000
 
+
+from Bio import Entrez
+import numpy as np
+
+Entrez.email = 'eopchang@gmail.com'
 handle = Entrez.esearch(db='pubmed', 
                             sort='relevance', 
                             retmax= '10000000',#str(no_max),
                             retmode='xml', 
-                            term=query)
-                            
+                            term=query)                            
 results = Entrez.read(handle)
-
 id_list = results['IdList']
 
+#chunk 로 쪼개기
 n_id = len(id_list)
 n_groups = int(np.ceil(n_id/size))
 id_list_new =  np.zeros((n_groups,size), dtype = int)
@@ -35,11 +42,14 @@ for i in range(n_groups):
             id_list_new[i,j] = id_list.pop(0)
        except IndexError: 
             pass
-                
-results_2 = []        
+              
+        
+results_2 = []#패칭 결과 담을 리스트     
 
-
-for i in range(0, n_groups): #네트워크 에러로 끈길 경우 0을 끊긴 시점의 i 값으로 대체하고 for 구문 다시 수행
+            
+            
+########FETCHING##############
+for i in range(87, n_groups): #네트워크 에러로 끈길 경우 0을 끊긴 시점의 i 값으로 대체하고 for 구문 다시 수행
     print(i+1, ' of ', n_groups)
     chunk = id_list_new[i,:]
     chunk = list(chunk[chunk > 0]) # 마지막 그룹은 ID자리에 0 이 있을수 있으므로.
