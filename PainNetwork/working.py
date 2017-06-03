c13c1b9000bddab9fc9a60eea728b097e27ba0d2
Chang-Eop Kim <eopchang@gmail.com>
2017-06-03 23:56:27 +0900

    1. FreqCandis_ROI_Brede.csv저장시 컬럼명 추가하도록 수정 2. freq 확인결과 명백히 지우는게 맞다고 판단되는 노드들 우선 제거 3. texts에서 \n 제거 4.texts를 정리하여 text_lists 생성:       texts는 abstract가 쪼개어져 있는 경우가 있으므로 이를 다시 정리함. -> 요소 하나는 하나의 paper 정보 포함하도록 (year, abstract)가 되도록. 5. occur_matrix (abstract별로 np.array (row: abstracts, col: year, freq. of nodes)) 생성. 대략 11시간 소요 예상. 아직 돌려보진 않은 상태.

--- PainNetwork/working.py
+++ PainNetwork/working.py
@@ -18,7 +18,7 @@ import pandas as pd
 import codecs
 import csv
 import re
-
+import time
 
 
 
@@ -121,7 +121,6 @@ for candi in candi_total:
                     if not (pattern_1.search(test_phrase)) and not (pattern_2.search(test_phrase)):
                         # 앞뒤로 알파벳 있지 않은 경우만 카운트
                         counts += 1
-                        print(text)
                         break #하나라도 찾아서 카운트했으면 다음 텍스트로 넘어감
        
                 
@@ -132,8 +131,12 @@ for candi in candi_total:
 #결과 csv 포맷으로 저장
 f2 = open('FreqCandis_ROI_Brede.csv', 'w')
 w = csv.writer(f2)
-for row in freq_of_candis.items():
-    w.writerow(row)
+
+for r_i, row in enumerate(freq_of_candis.items()):
+    if r_i == 0:
+        w.writerow(('Node', 'Freq')) #pandas 대비하여 컬럼 명 추가
+    else:
+        w.writerow(row)
 f2.close()
 
 
@@ -141,16 +144,119 @@ f2.close()
          
 
         
- #이하 작업중 시험 코드       
+#이하 작업중 시험 코드 
+nodes = pd.read_csv('FreqCandis_ROI_Brede.csv')   
+nodes = nodes.sort_index(by = 'Freq', ascending=False)
+
+nodes = nodes[nodes.Node != 'Trochlear nucleus'] #동의어 4 때문에 오류
+nodes = nodes[nodes.Node != 'Motor cortex'] # motor area 동의어
+nodes = nodes[nodes.Node != 'Primary motor cortex'] # motor area 동의어
+nodes = nodes[nodes.Node != 'Hippocampal formation']  # hippocampus 동의어
+nodes = nodes[nodes.Node != 'Cerebrospinal fluid'] # 적절치 않다고 판단
+nodes = nodes[nodes.Node != 'Archicortex'] #동의어에 hippocampus가 있어 빈도 높게 나오는것으로 보임.
+
+nodes = nodes[nodes.ix[:,1] > 20] # 일정 빈도 이상만.
+
+
+# texts에서 \n 제거 (시간 오래 걸림)
+ns = texts.count('\n')
+for i in range(ns): #'\n' in texts:
+    texts.remove('\n')
+    print(i+1, " of ", ns)
  
+# 이유 알수 없지만 '.\n'이 리스트에 섞여 있음.    
+for i in range(texts.count('.\n')):
+    texts.remove('.\n')
+    
+    
+
+
+
+#texts를 text_lists로 정리.
+#texts는 abstract가 쪼개어져 있는 경우가 있으므로 이를 다시 정리함. 
+#요소 하나는 하나의 paper 정보 포함하도록 (year, abstract)가 되도록.
+text_list = [] #엘리먼트 개별  (임시적으로 assign)
+text_lists = [] 
+for i, text in enumerate(texts):
+    print(i+1, len(texts))
+    try:
+        year = int(text)
+        text_lists.append(text_list)
+        text_list = [year]
+    except ValueError:
+        text_list.append(text)
+        
+text_lists.append(text_list) #맨 마지막은 따로 추가해야 함
+text_lists.remove([]) #제일 첫 엘리먼트가 공리스트라 제거.
+
+                 
+t_start = time.time()  
+#occur_matrix 생성
+#abstract별로 np.array (row: abstracts, col: year, freq. of nodes) 생성
+occur_matrix = np.zeros((len(text_lists), len(nodes)+1))
+for i, year_abstract in enumerate(text_lists):
+    print(i+1, " of ", len(text_lists))
+    occur_matrix[i,0] = year_abstract[0] #year
+    
+    #            
+    for j, node in enumerate(nodes.Node):
+        node = node.lower()
+        
+        #노도의 동의어 목록으로 쿼리 구성
+        for t in range(len(terms)):
+            if node in terms[t]: #동의어 목록에 있으면 
+                query = terms[t] #동의어 리스트를 쿼리로
+                break #찾아으면 다음 루프로
+            else: #동의어 목록에 없으면 
+                query = [node] #그냥 그 자체만 리스트 형태로
+                
+                
+                
+                
+                
+        #초록에 쿼리(동의어 포함) 등장 여부 결정(카운트 아닌 여부(1/0))
+        val = [] #동의어 각각의 등장 여부를 1/0 리스트로 만들것.
+        for qs in query:
+            
+            if qs in year_abstract[1]: #해당 용어 발견했을때
+                
+                pattern_0 = re.compile(qs) #용어 존재    
+                pattern_1 = re.compile('[a-z]'+qs)#용어 앞에 다른 문자 연결되어있거나
+                pattern_2 = re.compile(qs+'[a-z]')#용어 뒤에 다른 문자 연결된 경우라면
+                
+                iters = pattern_0.finditer(year_abstract[1]) #용어 존재하는 경우를  iterable 객체로 반환
+                
+                for it in iters: #용어 존재 경우 하나씩 돌아가면서
+                    start = it.span()[0] #텍스트 상에서의 시작위치
+                    end = it.span()[1] #텍스트 상에서의 용어 종료 위치
+                    test_phrase = year_abstract[1][start-1:end+1] #용어 앞뒤로 한칸 추가하여 따냄
+                    if not (pattern_1.search(test_phrase)) and not (pattern_2.search(test_phrase)):
+                        # 앞뒤로 알파벳 있지 않은 경우만 카운트
+                        val.append(1)
+                        break #하나라도 찾아서 카운트했으면 다음 텍스트로 넘어감
+        occur_matrix[i,j+1] = int(1 in val)                
+                
+    
+#plt.matshow(occur_matrix[:500,1:])
+
+t_end = time.time()          
+        
+print(t_end - t_start)         
+                    
+                
+
+      
+                 
+                 
+            
+
+
+
 
 
     
 
     
-    
-pattern = re.compile('[a-z]')
-pattern.se            
 
             
     
@@ -158,40 +264,6 @@ pattern.se
 
         
         
-for i in texts:
-    print(i, len(texts))
-    if i == '\n':
-        texts.remove(i)
-        
-for i in texts:
-    if 'after 2 h the animals were perfused. paraffin embedded brain sections immunoreacted with an antibody selective for' in i:
-        break
 
-
-
-
-
-
-
-results_2[0].keys()
-results_2[0]['MedlineCitation']['Article']['Abstract']
-
-
-
-
-
-data_2[data_2['Preferred Label'] == 'hypophysis']['Synonyms']
-#data_2.to_excel('neural_nodes_candidates.xlsx')
-
-
-
-
-flag = 0
-for i, j  in enumerate(texts):
-    print(i, len(texts))
-    if text in j:
-        flag = 1
-        print(1000000000)
-        
     
     
